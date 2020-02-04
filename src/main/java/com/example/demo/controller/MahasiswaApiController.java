d52163c1f548b4c577e1c80e48a0a20c7bf29136
Aas Asrini <asrinirpl07@gmail.com>
2020-02-04 17:22:16 +0700

    update get and delete

---
+++ src/main/java/com/example/demo/controller/MahasiswaApiController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+//import com.example.demo.model.Mahasiswa;
+import com.example.demo.services.MahasiswaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//import java.util.HashMap;
+//import java.util.Map;
+
+@RestController
+
+public class MahasiswaApiController {
+
+    private MahasiswaService mahasiswaService;
+
+    @Autowired
+    public void setMahasiswaService(MahasiswaService mahasiswaService) {
+        this.mahasiswaService = mahasiswaService;
+    }
+
+    @RequestMapping(value = "/mahasiswaapi")
+    public ResponseEntity<Object> getMahasiswa() {
+        return new ResponseEntity<>(mahasiswaService.listMahasiswa(), HttpStatus.OK);
+    }
+    
+    @RequestMapping(value="/mahasiswaapi/{id}")
+    public ResponseEntity<Object> getMahasiswaId(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(mahasiswaService.getIdMahasiswa(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/mahasiswaapi/{id}", method=RequestMethod.DELETE)
+        public ResponseEntity<Object> deleteMahasiswa(@PathVariable("id") Integer id) {
+        mahasiswaService.hapus(id);
+        return new ResponseEntity<>("Product is deleted successsfully",
+        HttpStatus.OK);
+    }
+    
+}
